84e6f012b4e7bd98f00ba8aa1457396a25abe339
GuoXiangDong <xiangdong.guo@classiclaw.com>
2019-03-09 19:00:05 +0800

    for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,13 @@ sh "ssh-add"
 lock "~> 3.11.0"
 
 set :application, "my_financial_report"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:BrightestStar/my_financial_report.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/my_financial_report"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -24,9 +24,13 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+set :passenger_restart_with_touch, true
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -36,6 +40,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
